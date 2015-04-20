ae4dbcc4a7640cb59fb69270f415d015bdfcfc25
Stan Le <stan.le1107@gmail.com>
2015-04-20 07:11:55 -0700

    Update MONTYHALL.js
    Pruned

--- MONTYHALL.js
+++ MONTYHALL.js
@@ -1,25 +1,12 @@
 function montyHall(bool){
   var guess = 0
-  //gonna be lazy and not change the door each time this door is real
-  var door = Math.floor((Math.random() * 3)+1)
+  var door = Math.floor((Math.random()*3)+1)
+  for (var i = 0; i <1000; i++){
+  var pick = Math.floor((Math.random() *3)+1)
+    if (bool === true){
+      if (door === pick){continue}
+      else{guess +=1}}
+    else if(door === pick){guess +=1}
 
-  //2 conditions to check for stats
-  if (bool === true){
-    for (var i = 0; i < 1000; i++){
-        //need to pick a door
-        var pick = Math.floor((Math.random() * 3)+1)
-        //possible outcomes
-        if (door === pick){continue;}
-        //switch doors
-        else {guess += 1}
-    }
   }
-  if (bool === false){
-    for (var i = 0; i < 1000; i++){
-      //just pick a door and print if its right or not
-      var pick = Math.floor((Math.random() * 3)+1)
-      if (door === pick){guess+=1}
-    }
-  }
-  return guess/1000 + "% correct"
-}
+  return guess/1000}
